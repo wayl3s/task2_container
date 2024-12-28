e9a82769bbe4e3f7d07f7a457c3655ebcbac4a7f
Wayl3s <kvlvsky7854@gmail.com>
2024-12-28 22:21:59 +0300

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -2,4 +2,4 @@
 sudo docker build --tag backup .
 sudo docker pull itzg/minecraft-server
 sudo docker run -d -p 25565:25565 -v ~/mc-server:/data -e EULA=TRUE -e ONLINE_MODE=false --name mc-server itzg/minecraft-server:latest
-sudo docker run -d -v ~/mc-server:/input -v ~/mc-server-backups:/output --name mc-backup backup:latest mc-server 60
+sudo docker run -d -v ~/mc-server:/input -v ~/mc-server-backups:/output --name mc-backup backup:latest mc-server 60 5
